8509b39ca1c5bbe08fa45e364beadfbcdd0de623
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-05-23 21:52:24 +0500

    Data types in python

---
+++ Fundamentals/dataTypes.py
@@ -0,0 +1,44 @@
+#DATA TYPES 
+#INTEGER (MAX RANGE 1 * 10 ** 308)
+age = 19 ;
+num = -1 ;
+print(age) ;
+print(age + num) ;
+# true division 
+dividend = 25 
+divisor = 4 
+print(dividend / divisor) 
+# floor division 
+print(dividend // divisor) 
+# raise to power
+num = 2   ; power = 3 
+print(num ** power) 
+
+# FLOAT DATATYPE 
+#  MAX RANGE 1.7e308
+print(1.7e308)
+# Boolean 
+print(True , end= ' && ')  ; print(False)
+# String
+mystring = 'Hello'
+print(mystring)
+#Complex  no.
+st = 4 + 5j
+print(st) 
+
+# LIST (in cor c++ ARRAY)
+print ([2,3,4,5])
+arr = [1,2,3,4,5,6,7]
+print(arr)
+#Tuple
+print((2,3,4))
+
+#Sets(unique elements )
+print({1,1,2,3}) # outout -> {1,2,3}
+
+# DICTIONARY(v . imp data type)
+print({'name' :'Abdul' , 'degree':'CS' , 'batch':'CSF25'})
+
+# function ->  TYPE
+print(type(True))
+print(type(3))
