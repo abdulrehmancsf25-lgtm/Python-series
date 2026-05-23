98281547f09aec14aa92aecf02b053a84b080b99
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-05-23 20:55:14 +0500

    Printing in python

---
+++ Fundamentals/helloWorld.py
@@ -0,0 +1,6 @@
+
+print('hello' , 4 , True , sep = '*')
+print("Second line" , end= '_')
+print("Again second line ")
+print('this is the third line ', ' hello world' , sep = '&&')
+print(True ,  2)
