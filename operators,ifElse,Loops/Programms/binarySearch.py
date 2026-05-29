ba28e6ef1b8e7ea5456b3a1d67cf494317b126de
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-05-29 15:31:53 +0500

    binary search in python loop practice

--- operators,ifElse,Loops/Programms/binarySearch.py
+++ operators,ifElse,Loops/Programms/binarySearch.py
@@ -4,18 +4,15 @@ target = int(input('Enter the target to search for '))
 s = 0 
 e = size -1 
 mid = s + (e-s)//2 
-find = False
 while s <= e :
   if arr[mid] == target :
-      find = True 
+      print('target found ')
       break 
   elif arr[mid] > target :
       e = mid -1 
   else :
        s = mid +1 
   mid = s + (e-s)//2
-
-if find :
-    print('target found')
+    
 else :
     print('not found ')
