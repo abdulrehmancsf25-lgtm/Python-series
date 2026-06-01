1830f4e1d7a4dc37448b047fc14b91e10f1c016c
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-06-01 23:03:51 +0500

    creating and accessing strings imp concepts

---
+++ loopsAndStrings.py/stringsImpCncpt.py
@@ -0,0 +1,45 @@
+"""
+import sys
+x = 'a'
+print(sys.getsizeof(x))
+print(len(x))
+st = '''abdul
+rehman
+arshad'''
+print(st)
+s = 129
+print(str(s))
+age = 19
+print("my age is "+str(age)+" years old")
+"""
+# POSITIVE INDEXING(0 TO SO ON LEFT --> RIGHT)
+name = 'abdul'
+print(name[0]) # print a
+
+# NEGATIVE INDEXING(-1 TO SO ON (LEFT <--- RIGHT))
+print(name[-1]) # print l
+
+# SLICING (MORE THAN ONE CHARCATERS )
+name  = 'hello world'
+print(name[0:5]) # index 0 to 4 will print out 
+# .find function returns first occurence index of a char in string else -1
+print('index of i ',name.find('i'))
+print('index of space ',name.find(' '))
+print('index of l ' ,name.find('l'))
+# .index also returns index 
+# if char not found not give -1 throughs value Error
+print(name.index('h'))
+'''
+for i in name:
+    if i == ' ':
+        print()'''
+# to print all indices of a cahracter 
+# i is for iterating and value stores that value in string 
+for i , value in enumerate(name):
+    if value == 'l':
+        print(i,end=' ')
+print()
+# if not want to use enumerate way (un - pythonic way )
+for i in range(len(name)):
+    if name[i] == 'l':
+        print(i," : ",name[i] )
